d15e0f7e16d9825739b1c9b7b4d92b6e3a74e286
RaghavL423 <97155764+RaghavL423@users.noreply.github.com>
2022-03-11 02:07:56 +0530

    Initial Commit

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+// let elements = document.querySelector('ul > li.click-menu')
+// console.log(elements)
+
+// $(document).ready(function() {
+
+//     $(".owl-carousel").owlCarousel();
+
+// });
+
+console.log("Hello Website")
